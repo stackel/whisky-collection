65ea018d3fba7b5061ae25eb68d2af938fc22308
Erik Stackelberg <stackelberg@splitstockholm.se>
2018-10-26 09:39:36 +0200

    react storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,30 +3,76 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 import {linkTo} from '@storybook/addon-links';
-import Temp from '../Temp';
 
-import {Button, Welcome} from '@storybook/react/demo';
+import '../index.css';
 
-storiesOf('Welcome', module).add(
-  'to Storybook',
-  () => <Welcome showApp={linkTo('Button')}/>
-);
+import whiskyList from 'data.json'
+import Name from 'components/whisky/Name'
+import Type from 'components/whisky/Type'
+import Country from 'components/whisky/Country'
+import Nose from 'components/whisky/Nose'
+import Palate from 'components/whisky/Palate'
+import Finish from 'components/whisky/Finish'
+import Rating from 'components/whisky/Rating'
+import ListItem from 'components/list/list-item/ListItem'
+import List from 'components/list/List'
+import Sort from 'components/list/sort/Sort'
+import Filter from 'components/list/filter/Filter'
 
-storiesOf('Button', module).add(
-  'with text',
-  () => <Button onClick={action('clicked')}>Hello Button</Button>
-).add('with some emoji', () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-));
+//export const whisky = whiskyList[0]
 
-export const temp = {
-  name: 'test',
-  type: 'test',
-  country: 'test'
+export const whisky = {
+  name: "2 Gingers Irish Whiskey",
+  cost: "$$",
+  class: "Scotch-like",
+  country: "Ireland",
+  type: "Blend",
+  nose: "Caramel",
+  palate: "Smoke",
+  finish: "Cinnamon"
+}
+
+export const actions = {
+  onNoseChange: action('onNoseChange'),
+  onPalateChange: action('onPalateChange'),
+  onFinishChange: action('onFinishChange'),
+  onRatingChange: action('onRatingChange'),
+  onCheckboxChange: action('onCheckboxChange'),
+  onSortChange: action('onSortChange'),
+  onFilterChange: action('onFilterChange')
 };
 
-storiesOf('Temp', module).add('temp', () => <Temp temp={temp}/>);
+storiesOf('Whisky', module).add('Name', () => <Name name={whisky.name}/>).add(
+  'Name (Large)',
+  () => <Name name={whisky.name} large="large"/>
+).add('Type', () => <Type type={whisky.type}/>).add(
+  'Type (Large)',
+  () => <Type type={whisky.type} large="large"/>
+).add('Country', () => <Country country={whisky.country}/>).add(
+  'Country (Large)',
+  () => <Country country={whisky.country} large="large"/>
+).add('Nose', () => <Nose nose={whisky.nose}/>).add(
+  'Nose (edit)',
+  () => <Nose nose={whisky.nose} edit="edit" onChange={actions.onNoseChange}/>
+).add('Palate', () => <Palate palate={whisky.palate}/>).add(
+  'Palate (edit)',
+  () => <Nose nose={whisky.palate} edit="edit" onChange={actions.onPalateChange}/>
+).add('Finish', () => <Finish finish={whisky.finish}/>).add(
+  'Finish (edit)',
+  () => <Finish finish={whisky.finish} edit="edit" onChange={actions.onFinishChange}/>
+).add('Rating', () => <Rating rating={whisky.rating}/>).add(
+  'Rating (edit)',
+  () => <Rating rating={whisky.rating} edit="edit" onChange={actions.onRatingChange}/>
+);
+
+storiesOf('List', module).add('ListItem', () => <ListItem whisky={whisky}/>).add(
+  'ListItem (With checkbox)',
+  () => <ListItem
+    whisky={whisky}
+    checkbox
+    checked={true}
+    onCheckboxChange={actions.onCheckboxChange}/>
+)
+.add('List', () => <List listItems={whiskyList}/>)
+.add('Sort', () => <Sort sortBy={"name"} onChange={actions.onSortChange}/>)
+.add('Filter', () => <Filter onChange={actions.onFilterChange}/>);
